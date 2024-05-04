3b659e085fe2d17ac4af2a738f9c3f584b6089a2
Viviaon <159008678+Viviaon@users.noreply.github.com>
2024-05-04 23:13:02 +0200

    Add files via upload

---
+++ Pages/Series.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# Ajouter des pages de navigation (Accueil / Films / Séries)
+# Ajouter une barre de recherche 
+
+# Modifying the page title and icon
+st.set_page_config(page_title = "Le Senechal - Series", page_icon = ":tv:", layout = "wide")
