6051aad8e0bf73df966f3f6209313d6bfa15608e
Viviaon <159008678+Viviaon@users.noreply.github.com>
2024-05-22 15:33:31 +0200

    Delete Streamlit/pages/Series.py

--- Streamlit/pages/Series.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-import plotly.express as px
-
-# Ajouter des pages de navigation (Accueil / Films / Séries)
-# Ajouter une barre de recherche 
-
-# Modifying the page title and icon
-st.set_page_config(page_title = "Le Senechal - Series", page_icon = ":tv:", layout = "wide")
